1198da37da3fe2e8af0da393593734a0eb28e923
Mazene <mmazenezerguine@gmail.com>
2024-12-08 21:19:37 +0100

    fix navigation issue

--- lib/ui/screens/posts_screen/posts.screen.dart
+++ lib/ui/screens/posts_screen/posts.screen.dart
@@ -44,86 +44,92 @@ class _PostsScreenState extends State<PostsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text(
-          'Posts List',
-          style: TextStyle(
-            color: Colors.blueGrey,
-            fontSize: 32,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
+    return WillPopScope(
+      onWillPop: () async {
+        // Return a Future<bool>
+        return false; // Prevents back navigation
+      },
+      child: Scaffold(
         backgroundColor: Colors.black,
-        elevation: 0,
-      ),
-      body: BlocBuilder<PostsBloc, PostsState>(
-        builder: (context, state) {
-          if (state is PostsLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is PostsLoadedSuccess) {
-            final posts = state.postsList;
-            final postKeys = posts.keys.toList();
+        appBar: AppBar(
+          title: const Text(
+            'Posts List',
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 32,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          backgroundColor: Colors.black,
+          elevation: 0,
+        ),
+        body: BlocBuilder<PostsBloc, PostsState>(
+          builder: (context, state) {
+            if (state is PostsLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is PostsLoadedSuccess) {
+              final posts = state.postsList;
+              final postKeys = posts.keys.toList();
 
-            if (_visibleItems.length != posts.length) {
-              _visibleItems.clear();
-              _visibleItems.addAll(List.generate(posts.length, (_) => false));
-              _animateItems();
+              if (_visibleItems.length != posts.length) {
+                _visibleItems.clear();
+                _visibleItems.addAll(List.generate(posts.length, (_) => false));
+                _animateItems();
+              }
+
+              return ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  final postId = postKeys[index];
+                  final post = posts[postId]!;
+
+                  return AnimatedOpacity(
+                    duration: const Duration(milliseconds: 500),
+                    opacity: _visibleItems[index] ? 1.0 : 0.0,
+                    curve: Curves.easeIn,
+                    child: PostListItem(
+                      postId: postId,
+                      postTitle: post.title,
+                      postDescription: post.description,
+                      onTap: () => _redirectToPostDetailsScreen(postId),
+                    ),
+                  );
+                },
+              );
+            } else if (state is PostsLoadedError) {
+              return Center(
+                child: Text(
+                  state.errorMessage,
+                  style: const TextStyle(color: Colors.red, fontSize: 16),
+                ),
+              );
+            } else if (state is PostsListEmpty) {
+              return const Center(
+                child: Text(
+                  'No posts found!',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+              );
             }
-
-            return ListView.builder(
-              itemCount: posts.length,
-              itemBuilder: (context, index) {
-                final postId = postKeys[index];
-                final post = posts[postId]!;
-
-                return AnimatedOpacity(
-                  duration: const Duration(milliseconds: 500),
-                  opacity: _visibleItems[index] ? 1.0 : 0.0,
-                  curve: Curves.easeIn,
-                  child: PostListItem(
-                    postId: postId,
-                    postTitle: post.title,
-                    postDescription: post.description,
-                    onTap: () => _redirectToPostDetailsScreen(postId),
-                  ),
-                );
-              },
-            );
-          } else if (state is PostsLoadedError) {
-            return Center(
-              child: Text(
-                state.errorMessage,
-                style: const TextStyle(color: Colors.red, fontSize: 16),
-              ),
-            );
-          } else if (state is PostsListEmpty) {
             return const Center(
               child: Text(
-                'No posts found!',
+                'something went wrong ??!',
                 style: TextStyle(color: Colors.grey, fontSize: 16),
               ),
             );
-          }
-          return const Center(
-            child: Text(
-              'something went wrong ??!',
-              style: TextStyle(color: Colors.grey, fontSize: 16),
-            ),
-          );
-        },
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _redirectToPostDetailsScreen(null);
+          },
+          backgroundColor: Colors.blueGrey,
+          child: const Icon(Icons.add, size: 28, color: Colors.white),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _redirectToPostDetailsScreen(null);
-        },
-        backgroundColor: Colors.blueGrey,
-        child: const Icon(Icons.add, size: 28, color: Colors.white),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
